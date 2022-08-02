f7cf6e30e97d3e145014037ca12ab432e1b26615
rahulnaikcs <rahul2691.naik@gmail.com>
2022-08-02 22:46:59 +0530

    Final Code Drawer Demo

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen.shade400,
+          title: const Text('List'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () => debugPrint("Item present"),
+              icon: const Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () => debugPrint("Member Added present"),
+              icon: const Icon(Icons.add_box),
+            ),
+            IconButton(
+              onPressed: () => debugPrint("Select Add Call"),
+              icon: const Icon(Icons.add_call),
+            ),
+          ],
+        ),
+        drawer: Drawer(
+            child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.red),
+              child: Text("List Settings"),
+            ),
+            ListTile(
+              title: const Text("Active Member"),
+              onTap: () => debugPrint("Active Member Selected"),
+            ),
+            ListTile(
+              title: const Text("Lazy Member"),
+              onTap: () => debugPrint("Lazy Member Selected"),
+            )
+          ],
+        )),
+      ),
+    );
+  }
+}
